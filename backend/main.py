a55a5d8ce56b6e452127867c6fb391cb2c5e4206
OumaymaElarbaoui04 <elarbaouioumayma8@gmail.com>
2025-05-22 23:30:03 +0100

    mise a jour

--- backend/main.py
+++ backend/main.py
@@ -1,22 +1,7 @@
-from fastapi import FastAPI, File, UploadFile
-import whisper
-import uvicorn
-import os
+from fastapi import FastAPI
 
 app = FastAPI()
 
-# Charger le modèle Whisper (base, small, medium, large, etc.)
-model = whisper.load_model("tiny")
-
-@app.post("/transcribe/")
-async def transcribe_audio(file: UploadFile = File(...)):
-    temp_path = f"temp_audio.{file.filename.split('.')[-1]}"
-    with open(temp_path, "wb") as buffer:
-        buffer.write(await file.read())
-
-    # Forcer la langue arabe (mieux pour transcrire la darija)
-    result = model.transcribe(temp_path, language="ar")
-
-    os.remove(temp_path)
-
-    return {"transcription": result["text"]}
+@app.get("/")
+def root():
+    return {"message": "Bienvenue sur mon API FastAPI sur Render"}
